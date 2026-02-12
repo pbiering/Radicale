6bcda96dc28035b43233ef66dda4c288a0420ed4
Peter Bieringer <pb@bieringer.de>
2026-02-12 22:41:04 +0100

    fix typing

--- radicale/sharing/none.py
+++ radicale/sharing/none.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Radicale.  If not, see <http://www.gnu.org/licenses/>.
 
+from typing import Union
+
 from radicale import sharing
 from radicale.log import logger
 
@@ -24,13 +26,13 @@ class Sharing(sharing.BaseSharing):
         """ dummy initialization """
         return True
 
-    def get_sharing_collection_by_token(self, token: str) -> [dict | None]:
+    def get_sharing_collection_by_token(self, token: str) -> Union[dict | None]:
         """ retrieve target and attributs by token """
         # default
         logger.debug("TRACE/sharing_by_token: 'none' cannot provide any map for token: %r", token)
         return None
 
-    def get_sharing_collection_by_map(self, path) -> [dict | None]:
+    def get_sharing_collection_by_map(self, path) -> Union[dict | None]:
         """ retrieve target and attributs by map """
         logger.debug("TRACE/sharing_by_map: 'none' cannot provide any map for path: %r", path)
         return {"mapped": False}
